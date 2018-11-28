f1bce9c8910733636cc890bc2dceed0b04ca0c94
gitmatheo <matteuszd@gmail.com>
2018-11-28 14:10:47 +0100

    Updates

---
+++ precache-manifest.0d07812ddb2c0cdc0f94aa7d816f4438.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bd2eb48bc79ce71ea7a",
+    "url": "/react-website/static/js/runtime~main.4bd2eb48.js"
+  },
+  {
+    "revision": "a7c1d1e9c3ef6d34f16c",
+    "url": "/react-website/static/js/main.a7c1d1e9.chunk.js"
+  },
+  {
+    "revision": "99020b87ecff4da1ed90",
+    "url": "/react-website/static/js/1.99020b87.chunk.js"
+  },
+  {
+    "revision": "a7c1d1e9c3ef6d34f16c",
+    "url": "/react-website/static/css/main.58cd5dd0.chunk.css"
+  },
+  {
+    "revision": "99020b87ecff4da1ed90",
+    "url": "/react-website/static/css/1.a0cb31ae.chunk.css"
+  },
+  {
+    "revision": "f598ed73d84a8531cf6a68c20a98c764",
+    "url": "/react-website/index.html"
+  }
+];
